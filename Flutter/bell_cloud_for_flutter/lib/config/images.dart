b4c91447c336050c99e2ec37ad869ffdb52953b0
wucanlong <wucanlong@bell.ai>
2020-06-24 17:15:46 +0800

    bell

---
+++ Flutter/bell_cloud_for_flutter/lib/config/images.dart
@@ -0,0 +1,5 @@
+class BImage{
+  static const String loginBg = 'assets/images/loginBg.png';
+  static const String phone = 'assets/images/phone.png';
+  static const String vCode = 'assets/images/vCode.png';
+}
