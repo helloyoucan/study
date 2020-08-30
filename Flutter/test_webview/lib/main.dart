1715e1de41485258d2e1c1fdbc084099c3ef53b4
helloyoucan <helloyoucan@163.com>
2020-08-30 16:32:53 +0800

    update

---
+++ Flutter/test_webview/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]).then((_) {
+    runApp(MyApp());
+  }).catchError((onError){
+    print(onError);
+  });
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        width: double.infinity,
+        height: double.infinity,
+        //https://www.jianshu.com/p/4aabe453eb26
+        child: WebView(
+          initialUrl: "https://t400apitest.bell.ai/english_course/#/preview/all?coursewareId=70&versionId=323",
+          //JS执行模式 是否允许JS执行
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
