ce1a1b29c38be436aa9716adb57013ae575c2318
wucanlong <wucanlong@bell.ai>
2020-10-28 18:58:12 +0800

    update

--- Flutter/flutter_hybrid/FlutterHybridAndroid/app/src/main/java/com/helloyoucan/flutterhybridandroid/MainActivity.java
+++ Flutter/flutter_hybrid/FlutterHybridAndroid/app/src/main/java/com/helloyoucan/flutterhybridandroid/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import io.flutter.embedding.android.FlutterFragment;
+import io.flutter.embedding.android.FlutterView;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +23,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction tx  = getSupportFragmentManager().beginTransaction();
-                FlutterFragment fragment = FlutterFragment.withNewEngine().initialRoute("{name:'devio',dataList:['aa','bb','cc']}").build();
-                tx.replace(R.id.someContainer, fragment);
-                tx.commit();
+                /**
+                 * 方式一
+                 */
+//                FragmentTransaction tx  = getSupportFragmentManager().beginTransaction();
+//                FlutterFragment fragment = FlutterFragment.withNewEngine().initialRoute("{name:'devio',dataList:['aa','bb','cc']}").build();
+//                tx.replace(R.id.someContainer, fragment);
+//                tx.commit();
+                /**
+                 * 方式二
+                 */
+                FlutterView flutterView = new FlutterView(MainActivity.this);
+//                View flutterView  = Flutter.createView(MainActivity.this,getLifecycle(),"route1");
+                FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600,800);
+//                addContentView(flutterView,layout);
+                FrameLayout someContainer = findViewById(R.id.someContainer);
+                someContainer.addView(flutterView, layout);
+                // 关键代码，将Flutter页面显示到FlutterView中
+//                FlutterEngine flutterEngine = new FlutterEngine(MainActivity.this);
+//                flutterEngine.getNavigationChannel().setInitialRoute("/");
+//                flutterEngine.getDartExecutor().executeDartEntrypoint(
+//                        DartExecutor.DartEntrypoint.createDefault()
+//                );
+//                flutterView.attachToFlutterEngine(flutterEngine);
             }
         });
     }
