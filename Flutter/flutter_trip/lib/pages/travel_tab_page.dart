e632bdbf374cdbed507dc74af7e9f5cebb5ddd76
wucanlong <wucanlong@bell.ai>
2020-11-16 18:55:50 +0800

    '13-8'

--- Flutter/flutter_trip/lib/pages/travel_tab_page.dart
+++ Flutter/flutter_trip/lib/pages/travel_tab_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_trip/dao/travel_dao.dart';
 import 'package:flutter_trip/model/travel_model.dart';
+import 'package:flutter_trip/widget/webview.dart';
 
 const PAGESIZE = 10;
 const _TRAVEL_URL =
@@ -17,7 +18,8 @@ class TravelTabPage extends StatefulWidget {
   _TravelTabPageState createState() => _TravelTabPageState();
 }
 
-class _TravelTabPageState extends State<TravelTabPage> {
+class _TravelTabPageState extends State<TravelTabPage>
+    with AutomaticKeepAliveClientMixin {
   List<TravelItem> travelItems;
   int pageIndex = 1;
   @override
@@ -73,6 +75,10 @@ class _TravelTabPageState extends State<TravelTabPage> {
     });
     return filterItems;
   }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
 }
 
 class _TravelItem extends StatelessWidget {
@@ -83,8 +89,143 @@ class _TravelItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (item.article.urls != null && item.article.urls.length > 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => WebView(
+                url: item.article.urls[0].h5Url,
+                title: '详情',
+              ),
+            ),
+          );
+        }
+      },
+      child: Card(
+        child: PhysicalModel(
+          color: Colors.transparent,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _itemImage(),
+              Container(
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  item.article.articleTitle,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              _infoText(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _itemImage() {
+    return Stack(
+      children: <Widget>[
+        Image.network(item.article.images[0]?.dynamicUrl),
+        Positioned(
+          bottom: 8,
+          left: 8,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(5, 1, 5, 1),
+            decoration: BoxDecoration(
+              color: Colors.black54,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 3),
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                    size: 12,
+                  ),
+                ),
+                LimitedBox(
+                  maxWidth: 130,
+                  child: Text(
+                    _poiName(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  String _poiName() {
+    return item.article.pois == null || item.article.pois.length == 0
+        ? '未知'
+        : item.article.pois[0]?.poiName ?? '未知';
+  }
+
+  _infoText() {
     return Container(
-      child: Text('$index'),
+      padding: EdgeInsets.fromLTRB(6, 0, 6, 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              PhysicalModel(
+                color: Colors.transparent,
+                clipBehavior: Clip.antiAlias,
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  item.article.author?.coverImage?.dynamicUrl,
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                width: 90,
+                child: Text(
+                  item.article.author?.nickName,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.thumb_up,
+                size: 14,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 3),
+                child: Text(
+                  item.article.likeCount.toString(),
+                  style: TextStyle(fontSize: 10),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
